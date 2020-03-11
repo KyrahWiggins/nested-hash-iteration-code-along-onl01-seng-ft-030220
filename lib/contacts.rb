70ab0b62e2f4f4dbe6569f1443c731e6cf5fa0ea
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-11 15:32:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,7 +3,7 @@ require 'pry'
 contacts = {
   "Jon Snow" => {
     name: "Jon",
-    email: "jon_snow@thewall.we", 
+    email: "jon_snow@thewall.we",
     favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
         knows: nil
   },
@@ -13,35 +13,35 @@ contacts = {
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
 }
- 
+
 def list_1(hash)
   hash.each do |person, data|
     #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-    #to iterate over the "data" hash, we can use the following line: 
- 
+    #to iterate over the "data" hash, we can use the following line:
+
     data.each do |attribute, value|
       puts "#{attribute}: #{value}"
     end
   end
-end 
+end
 
 # list_1(contacts)
 
 def list_2(hash)
   hash.each do |person, data|
     #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-    #to iterate over the "data" hash, we can use the following line: 
- 
+    #to iterate over the "data" hash, we can use the following line:
+
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
           # here, each index element in an ice cream flavor string
           puts "#{flavor}"
-        end 
+        end
       end
     end
   end
-end 
+end
 
 # list_2(contacts)
 
@@ -50,7 +50,7 @@ def remove_strawberry(contacts)
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-     end 
+     end
     end
   end
 end
